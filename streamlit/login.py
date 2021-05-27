6b44befa7b48340422a971fe6f5992c7be39c960
furkanzengin <furkanzeen07@gmail.com>
2021-05-27 20:24:29 +0300

    Update login.py

--- streamlit/login.py
+++ streamlit/login.py
@@ -1,14 +1,20 @@
 import streamlit as st
 from google.cloud import firestore
+
+import hashlib as hasher
+
 def loginPage():
 
     st.header("Lütfen Giriş Yapınız")
     mailLogin = st.text_input("E-mail")
     passwdLogin = st.text_input("Şifre", type="password")
+    sifreleyici = hasher.sha256()
+    sifreleyici.update(passwdLogin.encode("utf-8"))
+    passwdLogin = sifreleyici.hexdigest()
     login = st.button("Giriş")
     if login:
         if (mailLogin == "") or (passwdLogin == "") :
-            st.error("Alanlar Boş olamaz!")
+            st.error("Hiçbir Alan Boş Kalamaz!")
         else:
             # Authenticate to Firestore with the JSON account key.
             db = firestore.Client.from_service_account_json("firestore-key.json")
@@ -18,11 +24,11 @@ def loginPage():
 
             # Then get the data at that reference.
             doc = doc_ref.get() 
-
             for i in range(1,len(doc)+1):
                 temp = db.collection("users").document(str(i))
                 temp = temp.get()
                 datas = temp.to_dict()
+                print(datas)
                 if (datas["mail"] == mailLogin) and (datas["pass"] == passwdLogin):
                     st.success("Giriş işleminiz başarılı! Anasayfaya gidebilirsiniz.")
                     return True
@@ -34,6 +40,8 @@ def loginPage():
     surname = st.text_input("Soy İsminiz")
     mailSignUp = st.text_input("Mail Adresiniz?")
     passwdSignUp = st.text_input("Şifreniz", type="password")
+    sifreleyici.update(passwdSignUp.encode("utf-8"))
+    passwdSignUp = sifreleyici.hexdigest()
     signup = st.button("Kayıt Ol")
     if signup:
         if (name == "") or (surname == "") or (mailSignUp == "") or (passwdSignUp == "") :
